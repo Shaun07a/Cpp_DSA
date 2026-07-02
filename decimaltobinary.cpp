364d49c2210828146574ff268ba79104a927206c
Shaun07a <shaunjoseph004@gmail.com>
2026-07-03 00:22:53 +0530

    Extra code added

--- decimaltobinary.cpp
+++ decimaltobinary.cpp
@@ -1,3 +1,66 @@
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "Enter a decimal number :";
+//     cin >> n;
+
+//     int ans = 0;
+//     int i = 0;
+//     while(n!=0){
+//         int bit = n & 1;
+
+//         ans = (bit * pow(10,i)) + ans;
+
+//         n = n >> 1;
+//         i++;
+//     }
+
+//     cout << "Answer is :" <<  ans << endl;
+// }
+
+//Decimal to Binary
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int ans = 0;
+//     int place = 1;
+
+//     while (n != 0) {
+//         int bit = n & 1;
+//         ans += bit * place;
+//         place *= 10;
+//         n >>= 1;
+//     }
+
+//     cout << ans;
+// }
+
+//Negative Decimal to Binary
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     for (int i = 31; i >= 0; i--) {
+//         cout << ((n >> i) & 1);
+//     }
+
+//     cout << endl;
+
+//     return 0;
+// }
+
+//Binary to Decimal
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -5,18 +68,17 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
+    int i = 0, ans = 0;
 
-    int answer = 0;
-    int i = 0;
     while(n!=0){
-        int bit = n & 1;
+        int digit = n % 10;
+        if(digit == 1){
+            ans = ans + pow(2, i);
+        }
 
-        answer = (bit * pow(10,i)) + answer;
-
-        n = n >> 1;
+        n = n / 10;
         i++;
     }
 
-    cout << "Answer is :" <<  answer << endl;
+    cout << ans << endl;
 }
-
