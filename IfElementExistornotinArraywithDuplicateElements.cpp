f9365c7aab3f67aca11afc4f873858097fc3db3e
Shaun07a <shaunjoseph004@gmail.com>
2026-07-15 02:00:06 +0530

    Questions on Rotated Arrays

---
+++ IfElementExistornotinArraywithDuplicateElements.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool search(vector<int> &arr, int k){
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    while(low <= high){
+        int mid = (low + high) / 2;
+        if(arr[mid] == k) return true;
+        if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
+        if(arr[low] <= arr[mid]){
+            if(arr[low] <= k && k<=arr[mid]){
+                high = mid - 1;
+
+            }else{
+                low = mid + 1;
+            }
+        }else{
+            if(arr[mid]<=k && k<=arr[high]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+    int target;
+    cin >> target;
+    int element = search(arr, target);
+    cout << "Does the element exist in the array : " << element << endl;
+    return 0;
+}
