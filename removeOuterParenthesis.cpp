dd2e4494fadfe6f0bf64032c2eb835a9597e3208
Shaun07a <shaunjoseph004@gmail.com>
2026-08-03 03:05:20 +0530

    The code to remove the outer parenthesis

---
+++ removeOuterParenthesis.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeOuterParenthesis(string s){
+    string result = "";
+    int level = 0;
+    for(char ch : s){
+        if(ch == '('){
+            if(level > 0) result += ch;
+
+            level++;
+        }
+        else if(ch == ')'){
+            level--;
+            if(level > 0) result += ch;
+
+        }
+    }
+
+    return result;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    string ans = removeOuterParenthesis(s);
+    cout << "The final answer is : " << ans;
+}
