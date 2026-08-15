fc03d5e5d06aa9696d388375043a3e77e8f27d4f
Shaun07a <shaunjoseph004@gmail.com>
2026-08-16 00:45:11 +0530

    Converting an array to LL

--- LinkedListIntro.cpp
+++ LinkedListIntro.cpp
@@ -17,6 +17,18 @@ class Node{
     }
 };
 
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 0; i < arr.size(); i++){
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
 int main(){
     vector<int> arr = {2, 5, 8, 7};
     // Node y = Node(arr[0], nullptr);
@@ -25,6 +37,8 @@ int main(){
     // Node* y = new Node(arr[0], nullptr);
     // cout << y;
 
-    Node* y = new Node(arr[0]);
-    cout << y->data;
+    // Node* y = new Node(arr[0]);
+    // cout << y->data;
+    Node* head = convertArr2LL(arr);
+    cout << head->data;
 }
