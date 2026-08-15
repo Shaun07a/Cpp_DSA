8a7b046d144adead38b324c7bed58f44d12ad756
Shaun07a <shaunjoseph004@gmail.com>
2026-08-16 00:53:10 +0530

    Length of the LL

--- LinkedListIntro.cpp
+++ LinkedListIntro.cpp
@@ -29,6 +29,17 @@ Node* convertArr2LL(vector<int> &arr){
     return head;
 }
 
+int LengthOfLL(Node* head){
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        temp = temp->next;
+        cnt++;
+    }
+
+    return cnt;
+}
+
 int main(){
     vector<int> arr = {12, 5, 8, 7};
     // Node y = Node(arr[0], nullptr);
@@ -42,8 +53,5 @@ int main(){
     Node* head = convertArr2LL(arr);
     // cout << head->data;
     Node* temp = head;
-    while(temp){
-        cout << temp->data <<" ";
-        temp = temp->next;
-    }
+    cout << LengthOfLL(head);
 }
