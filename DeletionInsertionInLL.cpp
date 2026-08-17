9745c4149b4d77b1b3038a16e716a23805727777
Shaun07a <shaunjoseph004@gmail.com>
2026-08-18 01:56:49 +0530

    Deletion and Insertion in LL

--- DeletionInsertionInLL.cpp
+++ DeletionInsertionInLL.cpp
@@ -164,10 +164,33 @@ Node* insertPosition(Node* head, int el, int k){
     return head;
 }
 
+Node* insertBeforeValue(Node* head, int el, int val){
+    if(head == NULL){
+        return NULL;
+    }
+
+    if(head->data == val){
+        return new Node(el, head);
+
+    }
+    Node* temp = head;
+    while(temp->next != NULL){
+        
+        if(temp->next->data == val){
+            Node* x = new Node(el, temp->next);
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
 int main(){
     vector<int> arr = {12, 5, 8, 7};
     Node* head = convertArr2LL(arr);
-    head = insertPosition(head, 100, 2);
+    head = insertBeforeValue(head, 100, 12);
     print(head);
 
 }
