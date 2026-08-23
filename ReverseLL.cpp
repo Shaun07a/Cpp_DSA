0530b3106599c693abe194dcba2733c500f94901
Shaun07a <shaunjoseph004@gmail.com>
2026-08-23 21:57:19 +0530

    Reverse the LinkedList

--- ReverseLL.cpp
+++ ReverseLL.cpp
@@ -38,17 +38,31 @@ void print(Node* head){
     }
 }
 
+// Node* reverseLinkedList(Node* head){
+//     Node* temp = head;
+//     Node* prev = NULL;
+//     while(temp != NULL){
+//         Node* front = temp->next;
+//         temp->next = prev;
+//         prev = temp;
+//         temp = front;
+//     }
+
+//     return prev;
+// }
+
+//Recursive Approach
 Node* reverseLinkedList(Node* head){
-    Node* temp = head;
-    Node* prev = NULL;
-    while(temp != NULL){
-        Node* front = temp->next;
-        temp->next = prev;
-        prev = temp;
-        temp = front;
+    if(head == NULL || head->next == NULL){
+        return head;
     }
 
-    return prev;
+    Node* newHead = reverseLinkedList(head->next);
+    Node* front = head->next;
+    front->next = head;
+    head->next = NULL;
+
+    return newHead;
 }
 
 int main(){
