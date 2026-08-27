bd0fa7b8d9c3361c7d2ff12b68e7368426551f21
Shaun07a <shaunjoseph004@gmail.com>
2026-08-27 23:19:06 +0530

    Reverse Nodes in K Group Size of LL

--- reverseNodesInKGroupSizeOfLL.cpp
+++ reverseNodesInKGroupSizeOfLL.cpp
@@ -88,7 +88,8 @@ Node* kReverse(Node* head, int k){
 }
 
 int main(){
-    vector<int> arr = {1, 0 , 1, 2, 0, 2, 1};
+    vector<int> arr = {1, 2 , 3, 4, 5, 6, 7, 8, 9, 10};
     Node* head = convertArr2DLL(arr);
-    
+    Node* kReverseLL = kReverse(head, 3);
+    print(kReverseLL);
 }
