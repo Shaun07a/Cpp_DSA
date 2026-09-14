8a7fbee8f51c358c80786f87f500b81ac9fcecc8
Shaun07a <shaunjoseph004@gmail.com>
2026-09-14 16:05:00 +0530

    Binary Tree Representation

--- BinaryTreeRepresentation.cpp
+++ BinaryTreeRepresentation.cpp
@@ -12,9 +12,20 @@ struct Node{
     }
 };
 
+void preorder(Node* root){
+    if(root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
 int main(){
     struct Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->right = new Node(5);
+
+    preorder(root);
 }
